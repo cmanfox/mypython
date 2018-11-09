7f0f580bffbf353fbd35997ea5e6273a91395099
coldbise@gmail.com <coldbise@gmail.com>
2018-11-09 15:33:19 +0800

    df

--- connect_ora.py
+++ connect_ora.py
@@ -1,5 +1,5 @@
 import cx_Oracle                                          #引用模块cx_Oracle
-conn=cx_Oracle.connect('load/123456@localhost/ora11g')    #连接数据库
+conn=cx_Oracle.connect('system/Foxconn_2014#@10.138.4.53/GDSBGSTD')    #连接数据库
 c=conn.cursor()                                           #获取cursor
 x=c.execute('select sysdate from dual')                   #使用cursor进行各种操作
 x.fetchone()
